c372cd2d817323452f64743371336856051a1c76
Duminda de Silva <duminda.sanjaya.88@gmail.com>
2022-01-04 16:17:03 +0530

    minor change;

--- routes/s3Route.js
+++ routes/s3Route.js
@@ -2,16 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { getS3FileUrlsList, uploadFileToS3 } = require('../service/s3Service');
 
-router.get('/check_keys', async (req, res) => {
-  console.log('process.env.access_key_id :>> ', process.env.access_key_id);
-  console.log(
-    'process.env.secret_access_key :>> ',
-    process.env.secret_access_key
-  );
-  console.log('process.env.BUCKET_NAME :>> ', process.env.BUCKET_NAME);
-  res.status(200).send('');
-});
-
 router.get('/list', async (req, res) => {
   const objectUrlList = await getS3FileUrlsList();
   res.send(objectUrlList);
