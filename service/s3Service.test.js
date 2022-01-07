65b736776f41109ee4e470eaf0aa0a0b722fa9ff
Duminda de Silva <duminda.sanjaya.88@gmail.com>
2022-01-07 20:02:39 +0530

    added unit test;

---
+++ service/s3Service.test.js
@@ -0,0 +1,12 @@
+const { getS3FileUrlsList } = require('./s3Service');
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+
+jest.setTimeout(15000);
+
+describe('testing s3 file-url testing', () => {
+  test('checking if service call fetches objects', async () => {
+    const objectUrls = await getS3FileUrlsList();
+    expect(objectUrls.length).toBeGreaterThan(1);
+  });
+});
